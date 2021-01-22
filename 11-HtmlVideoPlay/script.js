57d2fad49af873e55b114178b8ab936c626581d3
Thigo <thiago.l17@yahoo.com>
2021-01-22 02:38:41 -0300

    Day 11

--- 11-HtmlVideoPlay/script.js
+++ 11-HtmlVideoPlay/script.js
@@ -46,8 +46,8 @@ video.addEventListener('pause', updateButton);
 video.addEventListener('timeupdate', handleProgress);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(btn => btn.addEventListener('click', skip));
-ranges.forEach(range=>range.addEventListener('click', handleRangeUpdate));
-ranges.forEach(range=>range.addEventListener('mousemove', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('click', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
 let mousedown = false;
 progress.addEventListener('click', scrub);
