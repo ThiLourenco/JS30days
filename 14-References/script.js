d422f513f60d4c3f775c9fb91f004f9380785b4a
Thigo <thiago.l17@yahoo.com>
2021-01-25 00:29:36 -0300

    Day 14

---
+++ 14-References/script.js
@@ -0,0 +1,79 @@
+// start with strings, numbers and booleans
+let age = 100;
+let age2 = age;
+console.log(age, age2);
+age = 200;
+console.log(age, age2);
+
+let name = "Thi";
+let name2 = name;
+console.log(name, name2);
+name = "ago";
+console.log(name, name2);
+
+// Let's say we have an array
+const players = ["Thi", "Moon", "Sun", "Loopy"];
+
+// and we want to make a copy of it.
+const team = players;
+console.log(players, team);
+
+// You might think we can just do something like this:
+team[5] = "Life";
+
+// however what happens when we update that array?
+
+// now here is the problem!
+
+// oh no - we have edited the original array too!
+
+// Why? It's because that is an array reference, not an array copy. They both point to the same array!
+
+// So, how do we fix this? We take a copy instead!
+const team2 = players.slice();
+const team3 = [].concat(players);
+
+// one way
+
+// or create a new array and concat the old one in
+
+// or use the new ES6 Spread
+const team4 = [...players];
+const team5 = Array.from(players);
+
+// now when we update it, the original one isn't changed
+
+// The same thing goes for objects, let's say we have a person object
+
+// with Objects
+const person = {
+  name: "Thiago",
+  age: 80
+};
+
+// and think we make a copy:
+const captain = person;
+captain.number = 99;
+
+// how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99, age: 12 });
+
+// We will hopefully soon see the object ...spread
+const cap3 = { ...person };
+
+// Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+const thi = {
+  name: "Thiago",
+  age: 99,
+  social: {
+    twitter: "@fake",
+    facebook: "fake.two"
+  }
+};
+
+// Not a deep clone
+const dev = Object.assign({}, thi);
+// Not a standard way but works
+const dev2 = JSON.parse(JSON.stringify(thi));
+
+console.warn('Check the comments in script');
